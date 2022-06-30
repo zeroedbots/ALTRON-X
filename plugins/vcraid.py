700d5ae6f4df851cfbfd9a4ff665187e0c6de4ef
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-06-30 21:05:00 +0530

    Update vcraid.py

--- plugins/vcraid.py
+++ plugins/vcraid.py
@@ -21,8 +21,8 @@ from pytgcalls.types.input_stream import InputAudioStream
 HNDLR = '/'
 
 aud_list = [
-    "./AltronUserbot/AUDIO1.ogg",
-    "./AltronUserbot/AUDIO2.ogg",
+    "./modules/AUDIO1.ogg",
+    "./modules/AUDIO2.ogg",
 ]
 
 
